35de747d22972323c5f0872c59c9c6a0674732a6
Kat Chuang <katychuang@gmail.com>
2013-06-06 14:37:50 -0400

    connect with PixelLogicPR api

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse
+import simplejson, urllib
+
 # Create your views here.
 
 def home(request):
+    url = "http://www.pixelogicpr.com/PRYouthAPI/public/api/counties"
+    counties = simplejson.load(urllib.urlopen(url))
 
     #pass to template
-    return render_to_response("pages/index.html")
+    return render_to_response("pages/index.html", {"counties": counties})
