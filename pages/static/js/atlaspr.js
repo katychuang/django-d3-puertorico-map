9feb23b36c3788183a7383ba8d0e36fc48ae53a6
Kat Chuang <katychuang@gmail.com>
2013-06-08 15:09:17 -0400

    add interactive data viz to /joven

--- pages/static/js/atlaspr.js
+++ pages/static/js/atlaspr.js
@@ -26,7 +26,7 @@ var AtlasPR = klass(function (options) {
   };
   this.size_attributes = {
     "small": {
-      width: 480,
+      width: 480
     },
     "medium": {
       width: 960
@@ -38,8 +38,8 @@ var AtlasPR = klass(function (options) {
       width: 2400
     }
   };
-  //this.width = this.size_attributes[this.options.size || "medium"].width;
-  this.width = this.options.size;
+  this.width = 750; //this.size_attributes[this.options.size || "medium"].width;
+  //this.width = this.options.size;
   this.height = this.width/3;
   this.original_scale = this.width*27;
   this.draw();
