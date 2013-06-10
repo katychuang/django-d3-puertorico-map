77cbb02751afc8f46232a4e4587c4d7792c8c1d5
katychuang <katychuang@gmail.com>
2013-06-10 17:15:14 -0400

    tables: ordered by county name, by value

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.db.models import Q
 from django.template import RequestContext
 import simplejson, urllib
-
+from operator import itemgetter
 
 #for contact form
 from django.shortcuts import render
@@ -30,27 +30,36 @@ def home(request):
 def search(request):
     api_url = "http://www.pixelogicpr.com/PRYouthAPI/public/api/"
     counties = simplejson.load(urllib.urlopen(api_url+"counties"))
-
-    categories = simplejson.load(urllib.urlopen(api_url+"categories"))
-
+    #categories = simplejson.load(urllib.urlopen(api_url+"categories"))
     indicators = simplejson.load(urllib.urlopen(api_url+"indicatorsList"))
 
     indicatorName = ''
     query_ind = ''
     filtered_ind = ''
     found_entries = None
+    jsonURL = ''
+    sortedValues = {}
+    counties = {}
 
     if ('indicators' in request.GET) and request.GET['indicators'].strip():
         query_ind = request.GET['indicators']
         filtered_ind = simplejson.load(urllib.urlopen(api_url + "indicators?id=" + query_ind))
-        print api_url + "indicators?id=" + query_ind
+
+        jsonURL = api_url + "indicators?id=" + query_ind
         indicatorName = filtered_ind[1]['indicator']
+
+    for item in filtered_ind:
+        counties[item['county_id']] = item['county']
+        sortedValues[item['county']] = item['value']
+
+    #json_string = simplejson.dumps(filtered_ind)
 
     return render_to_response('pages/index.html',
                               {"counties": counties,
-                                "categories": categories,
                                 "indicators": indicators,
                                 "valuesfordisplay": filtered_ind,
+                                "sorted": sorted(sortedValues.iteritems(), key=itemgetter(1), reverse=True),
+                                "jsonURL": jsonURL,
                                 "indicatorName": indicatorName},
                               context_instance=RequestContext(request))
 
